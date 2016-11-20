a843a4d2c0fa0c46c2035d6f95dada98c025a9df
Andrew Turley <AndyTurl@Andrews-MacBook-Air.local>
2016-11-20 20:21:00 +0000

    finished solution

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,7 +1,78 @@
-(function(){
+(function () {
     'use strict';
 
-    angular.module('NarrowItDownApp',[]);
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItemsDirective)
+        .constant('CONFIG', { url: 'https://davids-restaurant.herokuapp.com/menu_items.json' });
 
-    
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var search = this;
+
+        search.searchTerm = "";
+
+        search.narrowItDown = function () {
+            if (search.searchTerm && search.searchTerm.trim().length > 0) {
+                var promise = MenuSearchService.getMatchedMenuItems(search.searchTerm.trim());
+
+                promise.then(function (foundItems) {
+                    search.found = foundItems;
+                });
+            } else {
+                search.found = [];
+            }
+        };
+
+        search.removeItem = function (index) {
+            search.found.splice(index, 1);
+        };
+
+    };
+
+
+    MenuSearchService.$inject = ['$http', 'CONFIG'];
+    function MenuSearchService($http, CONFIG) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                url: CONFIG.url
+            }).then(function (result) {
+                var foundItems = [];
+                for (var i = 0; i < result.data.menu_items.length; i++) {
+                    if (result.data.menu_items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+                        foundItems.push(result.data.menu_items[i]);
+                    }
+                }
+                return foundItems;
+            });
+        }
+    };
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'founditems.html',
+            restrict: 'E',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            },
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'list',
+            bindToController: true
+        };
+
+        return ddo;
+    };
+
+    function FoundItemsDirectiveController() {
+        var list = this;
+
+        list.anyItems = function () {
+            return list.foundItems && list.foundItems.length > 0;
+        };
+    };
+
 })();
