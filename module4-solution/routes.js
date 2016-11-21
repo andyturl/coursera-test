a374ca7bb5239275fdf5a475c332bfbd5fb142b8
Andrew Turley <AndyTurl@Andrews-MacBook-Air.local>
2016-11-21 23:55:22 +0000

    almost finished solution 4

--- module4-solution/routes.js
+++ module4-solution/routes.js
@@ -7,6 +7,41 @@
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function RoutesConfig($stateProvider, $urlRouterProvider) {
-        
+
+        $urlRouterProvider.otherwise('home');
+
+        $stateProvider
+
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/home.template.html'
+            })
+
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'templates/categories.template.html',
+                controller: 'CategoriesController as ctrl',
+                resolve: {
+                    items: ['MenuDataService', function (MenuDataService) {
+                        console.log('getting categories');
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
+
+            .state('items', {
+                url: '/items/{category}',
+                templateUrl: 'templates/items.template.html',
+                controller: 'ItemsController as ctrl',
+                resolve: {
+                    items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+                        console.log('getting item');
+                        return MenuDataService.getItemsForCategory($stateParams.category);
+                    }]
+                }
+            });
+
+
+
     }
 })();
